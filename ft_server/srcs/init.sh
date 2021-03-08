61dc67708e0257ce8afdbaf5ad8abaa48512401d
Pasquale Barbaro <pbarbaro@e3r10p7.42roma.it>
2021-03-08 19:28:21 +0100

    finito

--- ft_server/srcs/init.sh
+++ ft_server/srcs/init.sh
@@ -3,8 +3,8 @@ service mysql start
 service php7.3-fpm start
 
 echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
-echo "CREATE USER 'marta'@'localhost' IDENTIFIED BY 'password';" | mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'marta'@'localhost';" | mysql -u root --skip-password
+echo "CREATE USER 'pbarbaro'@'localhost' IDENTIFIED BY '';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'pbarbaro'@'localhost';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 
 bash
